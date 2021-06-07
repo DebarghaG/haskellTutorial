87064190eb02cc70217a08d6fae336cd1533f620
DebarghaG <maildebargha@gmail.com>
2021-06-07 09:59:03 +0530

    Done, Bored

--- compiling.hs
+++ compiling.hs
@@ -6,4 +6,18 @@ main = do
   name <- getLine
   putStrLn ("Hello " ++ name)
 
--- 30 minutes in
+sayHello = do
+  putStrLn "What's your name"
+  name <- getLine
+  putStrLn $ "Hello" ++ name
+
+writeToFile = do
+  theFile <- openFile "test.txt" WriteMode
+  hPutStrLn theFile ("Random Line of Text")
+  hClose theFile
+
+readFromFile = do
+  theFile2 <- openFile "test.txt" ReadMode
+  contents <- hGetContents theFile2
+  putStr contents
+  hClose theFile2
--- playground.hs
+++ playground.hs
@@ -81,5 +81,109 @@ listTime4 = map times4 [1,2,3,4,5]
 multBy4 :: [Int] -> [Int]
 multBy4 [] = []
 multBy4 (x:xs) = times4 x : multBy4 xs
+-- So what's happening here? (This is quite confusing, really)
 
--- 46 mins in
+-- Haskell is amazing with recursion, writing program to do recursive string cehcking
+areStringsEq :: [Char] -> [Char] -> Bool
+areStringsEq [][] = True
+areStringsEq (x:xs)(y:ys) = x == y && areStringsEq xs ys
+areStringsEq _ _ = False
+
+-- Something more, we can recieve functions
+doMult :: (Int -> Int) -> Int
+doMult func = func 3
+num3Times4 = doMult times4
+
+-- We can receieve an integer, and return a function?
+getAddFunc :: Int -> (Int -> Int)
+getAddFunc x y = x + y
+adds3 = getAddFunc 3
+fourPlus3 = adds3 4
+threePlusList = map adds3 [1,2,3,4,5]
+
+--  Lambdas are just functions that don't have a name
+dbl1To10 = map (\x -> x*2) [1..10]
+
+-- Something more, an example of conditionals
+doubleEvenNumber y =
+  if (y `mod` 2 /= 0)
+    then y
+    else y*2
+
+getClass :: Int -> String
+getClass n = case n of
+  5 -> "Go to Kindergarten"
+  6 -> "Go to elementary School"
+  _ -> "Go Away"
+
+-- module SampFunctions (getClass, doubleEvenNumbers) where
+-- import SampFunctions
+
+-- An enumerated type
+data BaseballPlayer = Pitcher
+        | Catcher
+        | Infielder
+        | Outfield
+    deriving Show
+
+barryBonds :: BaseballPlayer -> Bool
+barryBonds Outfield = True
+barryInOf = print(barryBonds Outfield)
+
+
+-- Custom Data types
+data Customer = Customer String String Double
+  deriving Show
+
+iomSmith :: Customer
+iomSmith = Customer "Tom Smith" "123 Main" 20.50
+
+getBalance :: Customer -> Double
+getBalance (Customer _ _ b) = b
+
+-- Creating a new data type, for Rock paper Scissors
+data RPS = Rock | Paper | Scissors
+shoot :: RPS -> RPS -> String
+shoot Paper Rock = "Paper Beats Rock"
+shoot _ _ = "Error"
+-- Make all the alternatives of this
+
+-- Something more complex
+-- data Shape = Circle Float Float Float | Rectangle Float Float
+--     deriving Show
+
+-- area :: Shape -> Float
+
+-- area (Circle _ _ r) = pi * r ^ 2
+-- area (Rectangle x y x2 y2) = (abs $ x2 - x) * (abs $ y2 - y)
+
+-- sumValue = putStrLn ( show (1+2))
+-- sumValue2 = putStrLn . show $ 1 + 2
+-- areaOfCircle = area (Circle 50 60 20)
+
+
+
+-- TypeClasses inside haskell
+-- (+) Num means as long as typeClass num, it will work
+
+-- data Employee = Employee ( name :: String,
+--                           position :: String,
+--                         idNum :: Int) deriving (Eq, Show)
+
+-- We couldn't type functions starting with capital letters because that is for TypeClasses....
+
+-- data ShirtSize S | M | L
+
+-- instance Eq ShirtSize where
+--   S == S = True
+--   M == M = True
+--   L == L == True
+--   _ == + = False
+
+-- instance Show ShirtSize where
+--   show S = "Small"
+--   show M = "Medium"
+--   show L = "Large"
+
+-- smallAvail = S `elem` [S,M,L]
+-- theSize = show S
